84605b8befe632a668105850fe561aa12f033578
Andrea Fiore <and@inventati.org>
2011-06-01 11:20:12 +0100

    corrected innacurate documentation comment

--- test/lib/oknf-test-case.php
+++ test/lib/oknf-test-case.php
@@ -8,20 +8,20 @@ class OkfnTestCase extends PHPUnit_Framework_TestCase {
      * class                 - The name of the class to be mocked.
      * methods               - The array of instance method to be mocked.
      * constructor_params    - An array of parameters to be passed to the constructor.
-     * dont_call_constructor - whether or not to disable the constructor call; defaults to true (optional).
+     * call_constructor      - whether or not to call the constructor; defaults to true (optional).
      *
      *
      * returns a PHPUnit class instance mock.
      */
 
-    protected function mockHelper($class, $methods=array(), $constructor_params=array(), $dont_call_constructor=true) {
+    protected function mockHelper($class, $methods=array(), $constructor_params=array(), $call_constructor=true) {
 
       $mock_classname = OkfnUtils::unique_id($class . 'Mock');
       return $this->getMock($class,
         $methods,
         $constructor_params,
         $mock_classname,
-        $dont_call_constructor
+        $call_constructor
       );
     }
 
