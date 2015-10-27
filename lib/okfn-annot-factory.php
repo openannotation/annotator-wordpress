22837e0a87dfe858325ef22cb7c405a09dc1a849
Denis Obydennykh <denis.obydennykh@gmail.com>
2015-10-28 02:04:25 +0300

    updated settings page to match annotateIt auth protocol

--- lib/okfn-annot-factory.php
+++ lib/okfn-annot-factory.php
@@ -9,47 +9,31 @@
 
 class OkfnAnnotFactory extends OkfnBase {
   private $uri; //current wordpress page
-  private $annotator_content;
   private $template_vars;
   private $settings;
 
-  /*
+  /**
    * Public:
-   *
    * Class constructor
-   *
    * settings - an instance of OkfnAnnotSettings.
-   *
    */
-
   function __construct($settings) {
     $this->settings = $settings;
-    $this->annotator_content = $settings->get_option('annotator_content');
-
-    //todo: add load from search
-    $this->uri = $this->get_current_uri();
   }
 
 
-  /*
-   *
+  /**
    * Fetches the URI of the currently visited blog page
-   *
    * returns a uri string
-   *
    */
 
   private function get_current_uri(){
     return OkfnUtils::current_url();
   }
 
-  /*
+  /**
    * Wrapper method calling the Mustache template engine.
-   *
    * (Needed mostly for testing and mocking...)
-   *
-   *
-   *
    */
 
   function render_template($template_vars, $template='annotator-instance.js') {
@@ -60,15 +44,15 @@ class OkfnAnnotFactory extends OkfnBase {
 
 
   private function prepare_variables(){
-    $template_vars = array(
-      'annotator_content' => $this->annotator_content,
-      'uri' => $this->uri,
-    );
+    $template_vars = $this->settings->get_options_values();
+
+    //todo: add load from search
+    $template_vars['uri'] = $this->get_current_uri();
 
     return $template_vars;
   }
 
-  /*
+  /**
    * Generates a code snippet containing a customised Javascript instantiation
    * code for the Annotator.
    *
